809ef468f608b9b77d4921f225b12a1479d6cc9f
kaieaa <44272701+kaieaa@users.noreply.github.com>
2020-11-24 00:33:54 +0200

    groupExercises, index.js fixes

---
+++ API/services/groupExercisesService.js
@@ -0,0 +1,53 @@
+// Database mockup
+const groupExercises = [
+    {
+        id: 0,
+        dateAdded: Date.now(),
+        exerciseId: 0,
+        groupId: 0
+    },
+    {
+        id: 1,
+        dateAdded: Date.now(),
+        exerciseId: 0,
+        groupId: 0
+    }
+  ];
+  
+  const groupExercisesService = {};
+  
+  groupExercisesService.read = () => {
+    return groupExercises;
+  }
+  
+  
+  groupExercisesService.readById = (id) => {
+    return groupExercises[id];
+  }
+  
+  groupExercisesService.create = (groupExercise) => {
+    groupExercise.id = groupExercises.length,
+    groupExercises.push(groupExercise);
+    return groupExercise;
+  }
+  
+  groupExercisesService.update = (groupExercise) => {
+    // Check if optional data exists
+    if (groupExercise.description) {
+      // Change user data in 'database'
+      groupExercises[groupExercise.id].description = groupExercise.description;
+    }
+    // Check if optional data exists
+    if ((groupExercise.exerciseId || groupExercise.exerciseId === 0)) {
+      // Change user data in 'database'
+      groupExercises[groupExercise.id].exerciseId = groupExercise.exerciseId;
+    }
+    return groupExercises[groupExercise.id];
+  }
+  
+  groupExercisesService.delete = (id) => {
+    groupExercises.splice(id, 1);
+    return true;
+  }
+  
+  module.exports = groupExercisesService;
--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const pingController = require('./api/controllers/pingController');
 const usersController = require('./api/controllers/usersController');
 const studentsController = require('./API/controllers/studentsController');
 const exercisesController = require('./API/controllers/exercisesController');
-const studentExercisesController = require('./api/controllers/studentExercisesController');
+const groupsController = reuqire('.API/controllers/groupsController');
+const groupExercisesController = reuqire('.API/controllers/groupExercisesController');
+const studentTrainingsController = require('./API/controllers/studentTrainingsController');
+const studentTrainingExercisesController = require('./API/controllers/studentTrainingExercisesController');
 const notesController = require('./API/controllers/notesController');
 
 // Middleware required for receiving body from request object as JSON
@@ -35,11 +38,29 @@ app.post('/api/exercises', exercisesController.create);
 app.put('/api/exercises', exercisesController.update);
 app.delete('/api/exercises', exercisesController.delete);
 
-app.get('/api/studentExercises', studentExercisesController.read);
-app.get('/api/studentExercises/:id', studentExercisesController.readById);
-app.post('/api/studentExercises', studentExercisesController.create);
-app.put('/api/studentExercises', studentExercisesController.update);
-app.delete('/api/studentExercises', studentExercisesController.delete);
+app.get('/api/groups', groupsController.read);
+app.get('/api/groups/:id', groupsController.readById);
+app.post('/api/groups', groupsController.create);
+app.put('/api/groups', groupsController.update);
+app.delete('/api/groups', groupsController.delete);
+
+app.get('/api/groupExercises', groupExercisesController.read);
+app.get('/api/groupExercises/:id', groupExercisesController.readById);
+app.post('/api/groupExercises', groupExercisesController.create);
+app.put('/api/groupExercises', groupExercisesController.update);
+app.delete('/api/groupExercises', groupExercisesController.delete);
+
+app.get('/api/studentTrainings', studentTrainingsController.read);
+app.get('/api/studentTrainings/:id', studentTrainingsController.readById);
+app.post('/api/studentTrainings', studentTrainingsController.create);
+app.put('/api/studentTrainings', studentTrainingsController.update);
+app.delete('/api/studentTrainings', studentTrainingsController.delete);
+
+app.get('/api/studentTrainingExercises', studentTrainingExercisesController.read);
+app.get('/api/studentTrainingExercises/:id', studentTrainingExercisesController.readById);
+app.post('/api/studentTrainingExercises', studentTrainingExercisesController.create);
+app.put('/api/studentTrainingExercises', studentTrainingExercisesController.update);
+app.delete('/api/studentTrainingExercises', studentTrainingExercisesController.delete);
 
 app.get('/api/notes', notesController.read);
 app.get('/api/notes/:id', notesController.readById);
