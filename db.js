ceed78bd8d35aae2c53d22c8ad69b5fb0dd4f5c1
kaieaa <44272701+kaieaa@users.noreply.github.com>
2020-11-28 10:32:48 +0200

    config.js, db.js

---
+++ db.js
@@ -0,0 +1,7 @@
+const admin = require('firebase-admin');
+
+var serviceAccount = require('path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
