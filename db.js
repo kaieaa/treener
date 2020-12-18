67786ad17ccb1e86e5e92b64982ce6ee8d806914
kaieaa <44272701+kaieaa@users.noreply.github.com>
2020-12-18 14:56:17 +0200

    vea parandus require groupExercisesController

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
-const { dbConfig } = require('./config');
-// get the client
-const mysql = require('mysql2');
-const util = require('util');
- 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: dbConfig.host,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database,
-  timezone: 'local',
-  dateStrings: [
-		'DATE', // DATE's are returned as strings (otherwise they would be interpreted as YYYY-MM-DD 00:00:00+00:00)
-		'DATETIME' // DATETIME's return as strings (otherwise they would interpreted as YYYY-MM-DD HH:mm:ss+00:00)
-	]
-});
+  // Impordime config.js failist andmebaasiühenduse jaoks vajalikud muutujad
+  const { dbConfig } = require('./config')
+  // Impordime mysql2 teegi ja omistame selle mysql konstanti
+  const mysql = require('mysql2');
+  // Impordime Node util teegi selleks, et saaksime hiljem mysql andmebaasi ühendust kasutada sünkroonselt async/await-iga
+  const util = require('util');
 
+  // Loome andmebaasi ühenduse jaoks config objekti
+  const config = {
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
+};
+// Loome ühenduse andmebaasiga
+const connection = mysql.createConnection(config);
+// Muudame query callback funktsiooni Promise-ks
 connection.query = util.promisify(connection.query);
-
+// Ekspordime ühenduse
 module.exports = connection;
