791620ff25c1394a398b412641b5f5245371f99e
kaieaa <44272701+kaieaa@users.noreply.github.com>
2020-12-26 17:08:31 +0200

    usersService SQL select, insert, update, delete

--- API/controllers/usersController.js
+++ API/controllers/usersController.js
@@ -45,7 +45,7 @@ usersController.readById = async (req, res) => {
 // Returns:
 //  Success: status 201 - Created and user data in response body
 //  Fail: status 400 - Bad Request and error message in response body
-usersController.create = (req, res) => {
+usersController.create = async (req, res) => {
     // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
     const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
     const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
@@ -62,7 +62,7 @@ usersController.create = (req, res) => {
             password
         };
 
-        const newUser = usersService.create(user);
+        const newUser = await usersService.create(user);
         // Return data
         res.status(201).json({
             success: true,
@@ -84,7 +84,7 @@ usersController.create = (req, res) => {
 // Returns:
 //  Success: status 200 - OK and user data in response body
 //  Fail: status 400 - Bad Request and error message in response body
-usersController.update = (req, res) => {
+usersController.update = async (req, res) => {
     // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
     // Ternary operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
     const id = typeof(req.body.id) === 'number' ? req.body.id : false;
@@ -111,7 +111,7 @@ usersController.update = (req, res) => {
             password
         };
     
-        const updatedUser = usersService.update(user);
+        const updatedUser = await usersService.update(user);
             // Return updated user data
             res.status(200).json({
                 success: true,
@@ -133,11 +133,11 @@ usersController.update = (req, res) => {
 // Returns:
 //  Success: status 200 - OK and { success: true } message
 //  Fail: status 400 - Bad Request and error message in response body
-usersController.delete = (req, res) => {
+usersController.delete = async (req, res) => {
     // Check if required data exists
     const id = typeof(req.body.id) === 'number' ? req.body.id : false;
     if(id || id === 0) {
-        const result = usersService.delete(id);
+        const result = await usersService.delete(id);
         // Return success message
         res.status(200).json({
             success: result
