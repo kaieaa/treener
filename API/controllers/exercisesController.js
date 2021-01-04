c2b1b86b930cb3fd28dd95a3f896355a13597ab8
kaieaa <44272701+kaieaa@users.noreply.github.com>
2021-01-05 01:05:50 +0200

    exercisesService

--- API/controllers/exercisesController.js
+++ API/controllers/exercisesController.js
@@ -6,14 +6,21 @@ const exercisesController = {};
 // Required values: none
 // Optional values: none
 // Returns: status 200 - OK and list of exercises in response body
-exercisesController.read = (req, res) => {
-  // Get list of exercises
-  const exercises = exercisesService.read();
-  // Return list of exercises
-  res.status(200).json({
-      success: true,
-      exercises
+exercisesController.read = async (req, res) => {
+  const userId = req.user;
+  if (userId) {
+    // Get list of exercises
+    const exercises = await exercisesService.read(userId);
+    // Return list of exercises
+    res.status(200).json({
+        success: true,
+        exercises
+    });
+  } else {
+    res.status(400).json({
+      success: false
   });
+  }
 }
 
 // Endpoint for getting exercise specified by id
@@ -21,14 +28,30 @@ exercisesController.read = (req, res) => {
 // Required: id
 // Optional: none
 // Returns: status 200 - OK and exercise data in response body
-exercisesController.readById = (req, res) => {
-  const id = req.params.id;
-  const exercise = exercisesService.readById(id);
-  // Return exercise with specified id
-  res.status(200).json({
-      success: true,
-      exercise
+exercisesController.readById = async (req, res) => {
+  const userId = req.user;
+  const id = typeof(parseInt(req.params.id)) === 'number' ? parseInt(req.params.id) : false;
+  if (id) {
+    const exercise = await exercisesService.readById(id, userId);
+    if (exercise) {
+      // Return exercise with specified id
+      res.status(200).json({
+        success: true,
+        exercise
+      });
+    } else {
+      // Return error
+      res.status(400).json({
+        success: false,
+        message: 'No exercise found'
+      });
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      message: 'Required field(s) missing or invalid'
   });
+  }
 }
 
 // Endpoint for creating new exercise
@@ -38,26 +61,33 @@ exercisesController.readById = (req, res) => {
 // Returns:
 //  Success: status 201 - Created and lecturer data in response body
 //  Fail: status 400 - Bad Request and error message in response body
-exercisesController.create = (req, res) => {
+exercisesController.create = async (req, res) => {
   // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
   const name = typeof(req.body.name) === 'string' && req.body.name.trim().length > 0 ? req.body.name : false;
   const lecturerId = typeof(req.body.lecturerId) === 'number' ? req.body.lecturerId : false;
-  const userId = typeof(req.body.userId) === 'number' ? req.body.userId : false;
+  const userId = req.user;
 
   // Check if required data exists
-  if (name && (lecturerId || lecturerId === 0) && (userId || userId === 0)) {
+  if (name && lecturerId && userId) {
       // Create new json with user data
-      const newexercise = {
+      const exercise = {
           name,
-          lecturerId,
-          userId
+          lecturers_id: lecturerId,
+          users_id: userId
       };
-      const exercise = exercisesService.create(newexercise);
+      const id = await exercisesService.create(exercise);
       // Return data
-      res.status(201).json({
-          success: true,
-          exercise
-      });
+      if (id) {
+        res.status(201).json({
+            success: true,
+            id
+        });
+      } else {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong while creating new user'
+        });
+      }
   } else {
       // Return error message
       res.status(400).json({
@@ -72,21 +102,35 @@ exercisesController.create = (req, res) => {
 // Required: id
 // Optional: name, lecturerId
 // Returns:
-//  Success: status 200 - OK and exercise data in response body
+//  Success: status 200 - OK and exercise id
 //  Fail: status 400 - Bad Request and error message in response body
-exercisesController.update = (req, res) => {
+exercisesController.update = async (req, res) => {
   // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
   const id = typeof(req.body.id) === 'number' ? req.body.id : false;
   const name = typeof(req.body.name) === 'string' && req.body.name.trim().length > 0 ? req.body.name : false;
   const lecturerId = typeof(req.body.lecturerId) === 'number' ? req.body.lecturerId : false;
+  const userId = req.user;
   // Check if required data exists
-  if(id || id === 0) {
-      const exercise = exercisesService.update({ id, name, lecturerId });
+  if(id && userId) {
+      const exercise = {
+          id,
+          name,
+          lecturers_id: lecturerId,
+          users_id: userId
+      };
+      const result = await exercisesService.update(exercise);
       // Return updated user data
-      res.status(200).json({
-          success: true,
-          exercise
-      });
+      if (result) {
+        res.status(200).json({
+            success: result
+        });
+      } else {
+        res.status(400).json({
+            success: false,
+            message: 'exercise does not exists.'
+        });
+      }
+
   } else {
       // Return error message
       res.status(400).json({
@@ -103,15 +147,24 @@ exercisesController.update = (req, res) => {
 // Returns:
 //  Success: status 200 - OK and { success: true } message
 //  Fail: status 400 - Bad Request and error message in response body
-exercisesController.delete = (req, res) => {
+exercisesController.delete = async (req, res) => {
+    const userId = req.user;
   // Check if required data exists
-  const id = typeof(req.body.id) === 'number' ? req.body.id : false;
-  if(id || id === 0) {
-      const deleted = exercisesService.delete(id);
-      // Return success message
-      res.status(200).json({
-          success: deleted
-      });
+  const id = typeof(parseInt(req.body.id)) === 'number' ? parseInt(req.body.id) : false;
+  if(id && userId) {
+      const result = await exercisesService.delete(id, userId);
+      if (result) {
+        // Return success message
+        res.status(200).json({
+            success: result
+        });
+      } else {
+        res.status(400).json({
+            success: false,
+            message: 'No exercise found.'
+        });
+      }
+
   } else {
       // Return error message
       res.status(400).json({
--- API/services/exercisesService.js
+++ API/services/exercisesService.js
@@ -1,53 +1,43 @@
-// Database mockup
-const exercises = [
-    {
-        id: 0,
-        name: '',
-        lecturerId: 0,
-        userId: 0
-    },
-    {
-        id: 1,
-        name: 'Programmeerimine II',
-        lecturerId: 0,
-        userId: 0
-    }
-  ];
-  
-  const exercisesService = {};
-  
-  exercisesService.read = () => {
-    return exercises;
-  }
-  
-  exercisesService.readById = (id) => {
-    return exercises[id];
-  }
-  
-  exercisesService.create = (exercise) => {
-    exercise.id = exercises.length;
-    // Add lecturer to 'database'
-    exercises.push(exercise);
-    return exercise;
-  }
-  
-  exercisesService.update = (exercise) => {
-    // Check if optional data exists
-    if (exercise.name) {
-      // Change user data in 'database'
-      exercises[exercise.id].name = exercise.name;
-    }
-    // Check if optional data exists
-    if ((exercise.lecturerId || exercise.lecturerId === 0)) {
-        // Change user data in 'database'
-        exercises[exercise.id].lecturerId = exercise.lecturerId;
-    }
-    return exercises[exercise.id];
-  }
-  
-  exercisesService.delete = (id) => {
-    exercises.splice(id, 1);
-    return true;
-  }
-  
-  module.exports = exercisesService;
+const db = require('../../db');
+const exercisesService = {};
+
+exercisesService.read = async (users_id) => {
+  if (!users_id) return false;
+  const exercises = await db.query(`SELECT id FROM exercises WHERE users_id = ?`, [users_id]);
+  return exercises;
+}
+
+exercisesService.readById = async (id, users_id) => {
+  const exercises = await db.query(`SELECT name FROM exercises WHERE id = ? AND users_id = ?`, [id, users_id]);
+  if (exercises.length < 1 || exercises[0].users_id !== users_id) return false;
+  return exercises[0];
+}
+
+exercisesService.create = async (exercise) => {
+  if(!exercise) return false;
+  const result = await db.query(`INSERT INTO exercises SET ?`, [exercise]);
+  if (result.affectedRows === 0) return false;
+  return result.insertId;
+}
+
+exercisesService.update = async (exercise) => {
+  const exerciseToUpdate = await exercisesService.readById(exercise.id, exercise.users_id);
+  if (!exerciseToUpdate) return false;
+  if (exercise.name) exerciseToUpdate.name = exercise.name;
+  if (exercise.users_id) exerciseToUpdate.users_id = exercise.users_id;
+  // Remove fields not related to exercise (readById gives exercise with users data)
+  delete exerciseToUpdate.firstName;
+  delete exerciseToUpdate.lastName;
+  delete exerciseToUpdate.email;
+  const result = await db.query(`UPDATE exercises SET ? WHERE id = ? AND users_id = ?`, [exerciseToUpdate, exercise.id, exercise.users_id]);
+  if (result.affectedRows === 0) return false;
+  return true;
+}
+
+exercisesService.delete = async (id, users_id) => {
+  const result = await db.query(`DELETE FROM exercises WHERE id = ? AND users_id = ?`, [id, users_id]);
+  if (result.affectedRows === 0) return false;
+  return true;
+}
+
+module.exports = exercisesService;
