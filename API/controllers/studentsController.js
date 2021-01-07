73575aae1b9f5b704e2b912840a1ae32c72d97ae
kaieaa <44272701+kaieaa@users.noreply.github.com>
2021-01-07 17:40:17 +0200

    students

--- API/controllers/studentsController.js
+++ API/controllers/studentsController.js
@@ -1,144 +1,154 @@
 const studentsService = require('../services/studentsService');
-
 const studentsController = {};
 
-// Endpoint for getting list of available students
-// GET - students
+// Endpoint for getting list of available users
+// GET - users
 // Required values: none
 // Optional values: none
-// Returns: status 200 - OK and list of students in response body
-studentsController.read = (req, res) => {
-  // Get list of students
-  const students = studentsService.read();
-  // Return list of students
-  res.status(200).json({
-      success: true,
-      students
-  });
-};
+// Returns: status 200 - OK and list of users in response body
+studentsController.read = async (req, res) => {
+    const users = await studentsService.read();
+    // Return list of users
+    res.status(200).json({
+        success: true,
+        users: users
+    });
+}
 
-// Endpoint for getting student specified by id
-// GET - students
+// Endpoint for getting user specified by id
+// GET - users
 // Required: id
 // Optional: none
-// Returns: status 200 - OK and student data in response body
-studentsController.readById = (req, res) => {
-  const id = req.params.id;
-  // Get student from service
-  const student = studentsService.readById(id);
-  // Return student
-  res.status(200).json({
-      success: true,
-      student
-  });
-};
+// Returns: status 200 - OK and user data in response body
+studentsController.readById = async (req, res) => {
+    const userId = req.params.id;
+    if (userId) {
+        const user = await studentsService.readById(userId);
+        // Return user with specified id
+        res.status(200).json({
+            success: true,
+            user: user
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: 'No id provided'
+        });
+    }
+    
+}
 
-// Endpoint for creating new student
-// POST - students
-// Required values: firstName, lastName, email
-// Optional values: none
+// Endpoint for creating new user
+// POST - users
+// Required values: firstName, lastName, email, password
+// Optionalvalues: none
 // Returns:
-//  Success: status 201 - Created and student data in response body
+//  Success: status 201 - Created and user data in response body
 //  Fail: status 400 - Bad Request and error message in response body
-studentsController.create =(req, res) => {
-  // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
-  const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
-  const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
-  const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
-  const userId = typeof(req.body.userId) === 'number' ? req.body.userId : false;
+studentsController.create = async (req, res) => {
+    // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+    const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+    const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
+    const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 2 ? req.body.password : false;
 
-  // Check if required data exists
-  if (firstName && lastName && email && (userId || userId === 0)) {
-      // Create new json with user data
-      const newstudent = {
-          firstName,
-          lastName,
-          email,
-          userId
-      };
-      
-      const student = studentsService.create(newstudent);
-      // Return data
-      res.status(201).json({
-          success: true,
-          student
-      });
-  } else {
-      // Return error message
-      res.status(400).json({
-          success: false,
-          message: 'Required field(s) missing or invalid'
-      });
-  }
-};
+    // Check if required data exists
+    if (firstName && lastName && email && password) {
+        // Create new json with user data
+        const user = {
+            firstName,
+            lastName,
+            email,
+            password
+        };
 
-// Endpoint for updating student specified by id
-// PUT - students
+        const newUser = await studentsService.create(user);
+        // Return data
+        res.status(201).json({
+            success: true,
+            user: newUser
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+}
+
+// Endpoint for updating user specified by id
+// PUT - users
 // Required: id
-// Optional: firstName, lastName, email
+// Optional: firstName, lastName, email, password
 // Returns:
-//  Success: status 200 - OK and student data in response body
+//  Success: status 200 - OK and user data in response body
 //  Fail: status 400 - Bad Request and error message in response body
-studentsController.update = (req, res) => {
-  // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
-  const id = typeof(req.body.id) === 'number' ? req.body.id : false;
-  const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
-  const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
-  const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
-  // Check if required data exists
-  if(id || id === 0) {
-      // Check if optional data exists
-      if (firstName) {
-          // Change user data in 'database'
-          students[id].firstName = firstName;
-      }
-      // Check if optional data exists
-      if (lastName) {
-          // Change user data in 'database'
-          students[id].lastName = lastName;
-      }
-      // Check if optional data exists
-      if (email) {
-          // Change user data in 'database'
-          students[id].email = email;
-      }
-      // Return updated user data
-      res.status(200).json({
-          success: true,
-          student: students[id]
-      });
-  } else {
-      // Return error message
-      res.status(400).json({
-          success: false,
-          message: 'Required field(s) missing or invalid'
-      });
-  }
-};
+studentsController.update = async (req, res) => {
+    // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    // Ternary operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    /* Same as:
+    let id;
+     if (typeof(req.body.id) === 'number') {
+        id = req.body.id
+     } else {
+         id = false;
+     }
+     */
+    // Check if required data exists
+    if(id || id === 0) {
+        const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+        const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+        const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
+        const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 3 ? req.body.password : false;
+        
+        const user = {
+            id,
+            firstName,
+            lastName,
+            email,
+            password
+        };
+    
+        const updatedUser = await studentsService.update(user);
+            // Return updated user data
+            res.status(200).json({
+                success: true,
+                user: updatedUser
+            });
+    }  else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+}
 
-// Endpoint for deleting student specified by id
-// DELETE - students
+// Endpoint for deleting user specified by id
+// DELETE - users
 // Required: id
 // Optional: none
 // Returns:
 //  Success: status 200 - OK and { success: true } message
 //  Fail: status 400 - Bad Request and error message in response body
-studentsController.delete = (req, res) => {
-  // Check if required data exists
-  const id = typeof(req.body.id) === 'number' ? req.body.id : false;
-  if(id || id === 0) {
-      const deleted = studentsService.delete(id);
-      // Return success message
-      res.status(200).json({
-          success: true
-      });
-  } else {
-      // Return error message
-      res.status(400).json({
-          success: false,
-          message: 'Required field(s) missing or invalid'
-      });
-  }
-};
+studentsController.delete = async (req, res) => {
+    // Check if required data exists
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    if(id || id === 0) {
+        const result = await studentsService.delete(id);
+        // Return success message
+        res.status(200).json({
+            success: result
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+}
 
 module.exports = studentsController;
--- API/services/studentsService.js
+++ API/services/studentsService.js
@@ -1,46 +1,72 @@
-// Database mockup
-const students = [
-    {
-        id: 0,
-        firstName: 'Kalle',
-        lastName: 'Kuld',
-        email: 'kalle.kuld@tlu.ee',
-        userId: 0
-    },
-    {
-        id: 1,
-        firstName: 'Malle',
-        lastName: 'Muld',
-        email: 'malle.muld@tlu.ee',
-        userId: 0
-    },
-  ];
-  
-  const studentsService = {
-    read: () => {
-      return students;
-    },
-    readById: (id) => {
-      return students[id];
-    },
-    create: (student) => {
-      student.id = students.length,
-      // Add student to 'database'
-      students.push(student);
-  
-      const studentToReturn = { ... student };
-      delete password;
-  
-      return studentToReturn;
-    },
-    update: () => {
-  
-    },
-    delete: (id) => {
-      students.splice(id, 1);
-      return true;
+const hashService = require('./hashService');
+const db = require('../../db');
+
+studentsService = {};
+
+studentsService.read = async () => {
+  const users = await db.query(`SELECT id, firstName, lastName, phone, email FROM student`);
+  console.log(users);
+  return users;
+}
+
+// Return user by id
+studentsService.readById = async (id) => {
+  const users = await db.query(`SELECT id, firstName, lastName, phone, email FROM student WHERE id = ?`, [id]);
+  return users[0];
+}
+
+// Create student
+studentsService.create = async (student) => {
+  student.password = await hashService.hash(student.password);
+  // Add user to 'database'
+  const result = await db.query(`INSERT INTO student SET ?`, [student]);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return result.insertId;
+}
+
+studentsService.update = async (student) => {
+  const studentToUpdate = studentsService.readById(student.id);
+    // Check if optional data exists
+    if (student.firstName) {
+        // Change user data in 'database'
+        studentToUpdate.firstName = student.firstName;
     }
-  };
-  
-  
-  module.exports = studentsService;
+    // Check if optional data exists
+    if (student.lastName) {
+        // Change user data in 'database'
+        studentToUpdate.lastName = student.lastName;
+    }
+    // Check if optional data exists
+    if (user.phone) {
+      // Change user data in 'database'
+      studentToUpdate.phone = student.phone;
+    }
+    // Check if optional data exists
+    if (user.email) {
+        // Change user data in 'database'
+        studentToUpdate.email = student.email;
+    }
+    // Check if optional data exists
+    if (user.password) {
+        // Change user data in 'database'
+        studentToUpdate.password = hashService.hash(student.password);
+    }
+    
+    const result = await db.query(`UPDATE student SET ? WHERE id = ?`, [studentToUpdate, student.id]);
+    if (result.affectedRows === 0) {
+      return false;
+    }
+    return true;
+}
+
+studentsService.delete = async (id) => {
+  const result = await db.query(`DELETE FROM student WHERE id = ?`, [id]);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = studentsService;
