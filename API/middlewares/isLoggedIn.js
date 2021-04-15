3402c828258be25336cf43e25524a82e0c0ac5ec
kaieaa <74535489+kaieaa@users.noreply.github.com>
2021-04-15 13:39:42 +0300

    Create isLoggedIn.js

---
+++ API/middlewares/isLoggedIn.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const config = require("../../config");
+const isLoggedIn = async (req, res, next) => {
+  try {
+    //const token = req.headers.authorization ? req.headers.authorization.substring(7) : false;
+    const token = req.cookies.trainerSessionCookie;
+    const verified = token ? await jwt.verify(token, config.jwtSecret) : false;
+    console.log(verified.ID);
+    if (verified) {
+      req.user = verified.ID;
+      next();
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Invalid or missing token",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
+module.exports = isLoggedIn;
